41699d1201ca5de2558f09ba721dbfb4e90c4e75
Muhammad Umair <muhammadumair25591@gamil.com>
2025-04-02 02:59:06 +0500

    update

---
+++ assets/js/load_header.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#header-placeholder").load("./assets/components/header.html");
+});
+
+$(document).ready(function() {
+    $("#footer-placeholder").load("./assets/components/footer.html");
+});
---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// function updateNavbarClass() {
+//     const navContainer = document.getElementById("nav-container");
+
+//     if (window.innerWidth < 992) {
+//         navContainer.classList.add("container-fluid");
+//         navContainer.classList.remove("standard_container");
+//         navContainer.classList.remove("justify-content-between");
+//         navContainer.classList.remove("d-flex");
+//     } else {
+//         navContainer.classList.remove("container-fluid");
+//         navContainer.classList.add("standard_container");
+//         navContainer.classList.add("justify-content-between");
+//         navContainer.classList.add("d-flex");
+//     }
+// }
+
+// $(document).ready(function() {
+//     updateNavbarClass();
+// })
+// window.addEventListener("resize", updateNavbarClass);
