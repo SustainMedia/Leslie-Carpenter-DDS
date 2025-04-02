cadc026e730e5f31a88cce098cb374f0c18eae98
Muhammad Umair <muhammadumair25591@gamil.com>
2025-04-02 21:22:35 +0500

    update

--- assets/js/load_header.js
+++ assets/js/load_header.js
@@ -1,5 +1,27 @@
+function setActiveNavItem() {
+
+    console.log("function runs")
+
+    const currentPath = window.location.pathname.split('/').pop();
+    const navLinks = document.querySelectorAll('.nav-link1');
+
+    console.log(navLinks);
+    
+    navLinks.forEach(link => {
+
+        console.log("link not Found")
+
+        const linkPath = link.getAttribute('href').split('/').pop();
+        if (currentPath === linkPath) {
+            console.log("link Found")
+            link.closest('.nav-item').classList.add('active');
+        }
+    });
+}
+
 $(document).ready(function() {
     $("#header-placeholder").load("./assets/components/header.html");
+    setTimeout(setActiveNavItem, 100);
 });
 
 $(document).ready(function() {
